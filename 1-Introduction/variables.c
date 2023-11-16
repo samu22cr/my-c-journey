3b951a4587cd8acef679f1c770146368a2dfb773
samu22cr <samuelastuaflores@gmail.com>
2023-11-15 21:11:00 -0600

    Learning about pointers :'c

---
+++ 1-Introduction/variables.c
@@ -0,0 +1,32 @@
+
+// Variables
+
+#include <stdio.h>
+
+//int main(void) {
+	//int i = 2;
+	//float f = 3.14;
+	//char *s = "Hello World";
+//
+	//printf("%s i = %d and f = %f\n", s, i, f);
+//}
+
+int main(void) {
+	int x = 1;
+	int y = -321311;
+
+;
+	if (x) {
+		printf("x is true");
+	}
+	if (y) {
+		printf("y is true");
+	}
+	if (!z) {
+		printf("z is false");
+	} else {
+		printf("sdada");
+	}
+}
+
+
