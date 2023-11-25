8900512390cd6adb521bfb2fa014bdd9dcf8e03a
samu22cr <samuelastuaflores@gmail.com>
2023-11-25 15:56:39 -0600

    - Made a new mini-project to put into practice pointers, structs,   strings & file I/O. What I made was a simulation of an authentication   system, where users authenticated via username/password to a plain   text file

--- 7-file-io/files.c
+++ 7-file-io/files.c
@@ -2,6 +2,97 @@
 
 int main(void) {
 
+	// structs-and-numbers-caveats--------------------------------------------------
+	// caveats = "advertencias"
 
+	// binary-file-I/O--------------------------------------------------
+	
+	// read binary!
+	//FILE *fp;
+	//unsigned char c;
+	//fp = fopen("output.bin", "rb"); // rb mode for "read binary"
+	//while(fread(&c, sizeof(char), 1, fp) > 0) { // fread returns 0 on EOF
+		//printf("%d\n", c);
+	//}
+
+	// write binary!
+	//FILE *fp;
+	//unsigned char bytes[6] = { 7, 37, 0, 88, 255, 12 };
+	//fp = fopen("output.bin", "wb"); // wb mode for "write binary"
+	//// In the call to fwrite, the arguments are:
+	//// * Pointer to data to write
+	//// * Size of each "piece" of data
+	//// * Count of each "piece" of data
+	//// * FILE*
+	//fwrite(bytes, sizeof(char), 6, fp);
+	//fclose(fp);
+
+
+
+
+
+	// writting-text-files----------------------------------------------
+
+	//FILE *fp;
+	//int x = 32;
+	//fp = fopen("output.txt", "w"); // "w" ovewrites whole file
+	////fp = stdout ; // stdout is just a file (stream)
+	////fp = stderr ; // stderr is just a file (stream)
+	//fputc('B', fp);
+	//fputc('\n', fp); // just a simple newline char
+	//fprintf(fp, "x = %d\n",x);
+	//fputs("Hello, world!\n", fp);
+	//fclose(fp);
+
+
+	// formatted-input--------------------------------------------------
+	//FILE *fp;
+	//char name[1024]; // Big enough for any line this program will encounter
+	//float length;
+	//int tonnes;
+	//int counter = 0;
+	//fp = fopen("whales.txt", "r");
+	//while(fscanf(fp, "%s    %f %d	", name, &length, &tonnes) != EOF) {
+		//printf("WHALE #%d: name=\"%s\", length=%.1f meters, mass=%d tonnes\n", ++counter, name, length, tonnes);
+	//}
+	//fclose(fp);
+
+	// fscanf() return EOF on end-of-file
+
+	// reading-a-LINE-at-a-time-----------------------------------------
+	//FILE *fp;
+	//char s[1024];
+	//int linecount = 0;
+	//fp =  fopen("quote.txt", "r");
+	//while(fgets(s, sizeof s, fp) != NULL) {
+		//printf("%d: %s", ++linecount, s);
+	//}
+	//fclose(fp);
+	
+
+
+	// EOF(End of File)-------------------------------------------------
+	
+	// There is a special character defined as a macro: EOF.
+	// This is what fgetc() will return when the end of the
+	// file has been reached and you’ve attempted to read another character. 
+	//FILE *fp;
+	//int c;
+	//fp = fopen("hello.txt", "r");
+	//while( (c = fgetc(fp)) != EOF) { printf("%c", c); } 
+	//fclose(fp);
+	
+
+	// reading-text-files----------------------------------------------
+
+	//FILE *fp;
+	//fp =  fopen("hello.txt", "r"); // fopen() returns NULL if something goes wrong
+	//int c = fgetc(fp);
+	//printf("%c\n", c);
+	//fclose(fp);
+
+	// the FILE* data type----------------------------------------------
+
+	
 
 }
---
+++ projects/2-basic-persistent-login/login.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define NAME_LEN 20
+
+/*
+	- basic (non-secure) login should be able to:
+		- read users from file
+		- let user authenticate via username/password
+		- let user create accounts
+	- there isn't error checking on some stuff I still don't know
+	how to check (example: how to get strings with whitespaces when using scanf()
+*/
+
+
+struct user {
+	char *name;
+	char *password;
+};
+
+
+void print_home_menu(void) {
+	printf("---------------------------------------------------\n");
+	printf("0. Exit\n");
+	printf("1. Login\n");
+	printf("2. Create Account\n");
+	printf("---------------------------------------------------\n");
+}
+
+void print_logged_in_menu(void) {
+	printf("---------------------------------------------------\n");
+	printf("0. Exit\n");
+	printf("1. Log out\n");
+	printf("---------------------------------------------------\n");
+}
+
+
+void login_prompt(struct user *u) {
+
+	char username[NAME_LEN];
+	printf("Username: ");
+	scanf("%s", username);
+	u->name = strdup(username);
+
+	char password[NAME_LEN];
+	printf("Password: ");
+	scanf("%s", password);
+	u->password = strdup(password);
+}
+
+
+
+bool authenticate(struct user *u) {
+
+	FILE* fp;
+	fp = fopen("users.txt", "r");
+	char username[NAME_LEN];
+	char password[NAME_LEN];
+	int count = 0;
+
+	bool authd = false;
+	while (fscanf(fp, "%s %s", username, password) != EOF) {
+
+		bool same_name = strcmp(u->name, username) == 0;
+		bool same_password = strcmp(u->password, password) == 0;
+
+		if(same_name && same_password) {
+			authd = true;
+			printf("authenticated!!!\n");
+			break;
+		} 
+	}
+	if(!authd) {
+		printf("No usernames/passwords match, sorry...\n");
+	}
+	fclose(fp);
+	return authd;
+}
+
+
+void persist_user(struct user *u) {
+	FILE *fp;
+	fp = fopen("users.txt", "a");
+	fprintf(fp,"%s %s\n", u->name, u->password);
+	fclose(fp);
+}
+
+int main(void) {
+	
+	bool prompting = true;
+	bool session  = false; // 0 = unauthenticated, 1 = authenticated	
+
+	print_home_menu();
+	while(prompting) {
+
+		int mode;
+		printf("Option? (-1 for help): ");
+		scanf("%d", &mode);
+		struct user u;
+		
+		if(!session) {
+			switch(mode) {
+				case -1:
+					print_home_menu();
+					break;
+				case 0:
+					printf("Bye bye!\n");
+					prompting = false;
+					break;
+				case 1:
+					login_prompt(&u);
+					session = authenticate(&u);
+					if(session) {
+						print_logged_in_menu();
+					}
+					break;
+				case 2:
+					printf("Creating account...\n");
+					login_prompt(&u);
+					persist_user(&u);
+					session = true;
+					print_logged_in_menu();
+					break;
+				default: 
+					printf("Invalid option!\n");
+					break;
+				
+			}
+		} else {
+			switch(mode) {	
+				case -1:
+					print_home_menu();
+					break;
+				case 0:
+					printf("Bye bye!\n");
+					prompting = false;
+					break;
+				case 1:
+					printf("Logging out...!\n");
+					print_home_menu();
+					session = false;
+					break;
+			}
+		}
+		
+	}
+
+
+
+}
+
+
