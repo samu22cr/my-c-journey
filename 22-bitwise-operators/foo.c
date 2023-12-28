03368ed8b4abb64db409a5f354460a3573214f53
samu22cr <samuelastuaflores@gmail.com>
2023-12-28 17:14:38 -0600

    learning bout bitwise operators (just theory...)

---
+++ 22-bitwise-operators/foo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(void) {
+//	printf("%zu \n", sizeof(int));
+	 
+	// Bitwise operators--------------------
+
+	// Some commmon ones...
+	//AND 	& 	a = b & c
+	//OR 	| 	a = b | c
+	//XOR 	^ 	a = b ^ c
+	//NOT 	~ 	a = ~c
+
+	// Assignment shorthands
+	//&= 	a &= c 	a = a & c
+	//|= 	a |= c 	a = a | c
+	//^= 	a ^= c 	a = a ^ c
+	
+	// Other common ones...
+	//Shift left 	<< 	a = b << c
+	//Shift right 	>> 	a = b >> c
+	
+	// Assignment shorthands...
+	//>>= 	a >>= c 	a = a >> c
+	//><= 	a <<= c 	a = a << c
+
+}
